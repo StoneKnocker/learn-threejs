fa72a8bdd43526c8fe120d7bb2652e81adf6a8e6
xianyang <xianyang.lu@vcg.com>
2024-12-23 13:45:08 +0800

    load model

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css'
+import { Canvas, useLoader} from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei'
+import { Environment } from '@react-three/drei'
+import { Suspense } from 'react'
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
+
+const Model = () => {
+  const gltf = useLoader(GLTFLoader, './Poimandres.gltf')
+  return (
+    <>
+      <primitive object={gltf.scene} scale={0.4} />
+    </>
+  )
+}
+
+const App = () => (
+  <div className="w-full h-screen">
+    <Canvas>
+      <Suspense fallback={null}>
+        <Model />
+      <OrbitControls />
+      <Environment preset="sunset" background />
+    </Suspense>
+    <ambientLight color={0xff0000} intensity={0.1} />
+    <directionalLight position={[0, 0, 5]} intensity={0.5} />
+  </Canvas>
+  </div>
+)
+
+export default App
