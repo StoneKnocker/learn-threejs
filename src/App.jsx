b0bf01509c6e014ed4a11494b5e646a61e94e8dc
xianyang <xianyang.lu@vcg.com>
2024-12-26 22:13:04 +0800

    init

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,17 @@
 import './index.css'
-import { Canvas, useLoader} from '@react-three/fiber'
+import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Suspense } from 'react'
-import { TextureLoader } from 'three/src/loaders/TextureLoader'
-
-const name = (type) => `PavingStones092_1K-JPG/PavingStones092_1K-JPG_${type}.jpg`;
-
-const Scene = () => {
-  const [colorMap, displacementMap, normalMap, roughnessMap, aoMap] = useLoader(TextureLoader, [name('Color'), name('Displacement'), name('NormalGL'), name('Roughness'), name('AmbientOcclusion')])
-  return (
-    <>
-    <ambientLight intensity={0.2} />
-    <directionalLight/>
-    <mesh>
-      <sphereGeometry args={[1, 100, 100]}/>
-      <meshStandardMaterial 
-      map={colorMap}
-      displacementScale={0.2}
-      displacementMap={displacementMap}
-      normalMap={normalMap}
-      roughnessMap={roughnessMap}
-      aoMap={aoMap}
-      />
-    </mesh>
-    </>
-  );
-}
 
 const App = () => (
-  <Canvas className="w-full h-full">
+  <Canvas>
       <Suspense fallback={null}>
-        <Scene />
+        <ambientLight intensity={0.2} />
+        <directionalLight/>
+        <mesh>
+          <sphereGeometry args={[1, 100, 100]}/>
+          <meshStandardMaterial />
+        </mesh>
         <OrbitControls autoRotate/>
       </Suspense>
   </Canvas>
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Animatation from './Animatation.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <Animatation />
+    <App />
   </StrictMode>,
 )
