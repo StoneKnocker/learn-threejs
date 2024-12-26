f0f34d43e7b7ef714d3ae62042e9d9972222e66f
xianyang <xianyang.lu@vcg.com>
2024-12-26 23:18:36 +0800

    texutre worked

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
 import './index.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Sphere, Stage } from '@react-three/drei'
 import { Suspense } from 'react'
+import { useTexture } from '@react-three/drei'
+import { useControls } from 'leva'
+import * as THREE from 'three'
+
+function Box() {
+  const {args1} = useControls({
+    args1: [1,32,32]
+  })
+
+  const textureProps = useTexture({
+    map: 'textures/color.jpg',
+    displacementMap: 'textures/displacement.jpg',
+    metalnessMap: 'textures/metalness.jpg',
+    roughnessMap: 'textures/roughness.jpg',
+    normalMap: 'textures/normal.jpg',
+  })
+  return (
+  <Sphere args={args1}>
+    <meshPhysicalMaterial {...textureProps} map-magFilter={THREE.NearestFilter} displacementScale={0.5}/>
+  </Sphere>
+  )
+}
 
 const App = () => (
-  <Canvas>
+  <Canvas shadows dpr={[1,2]}>
+      <OrbitControls makeDefault autoRotate/>
       <Suspense fallback={null}>
-        <ambientLight intensity={0.2} />
-        <directionalLight/>
-        <mesh>
-          <sphereGeometry args={[1, 100, 100]}/>
-          <meshStandardMaterial />
-        </mesh>
-        <OrbitControls autoRotate/>
+        <Stage preset="portrait" shadows={false}>
+          <Box/>
+        </Stage>
       </Suspense>
   </Canvas>
 )
