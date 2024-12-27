459aca9d6b516d24ca4aa5d153153c0420793893
xianyang <xianyang.lu@vcg.com>
2024-12-27 14:42:19 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,18 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 import { Suspense } from 'react'
 
-const App = () => (
-  <Canvas>
-      <Suspense fallback={null}>
-        <ambientLight intensity={0.2} />
-        <directionalLight/>
-        <mesh>
-          <sphereGeometry args={[1, 100, 100]}/>
-          <meshStandardMaterial />
-        </mesh>
-        <OrbitControls autoRotate/>
-      </Suspense>
-  </Canvas>
-)
+const App = () =>{
+  return (
+    <Canvas>
+      <ambientLight intensity={0.2} />
+      <directionalLight/>
+      <mesh>
+        <sphereGeometry args={[1, 100, 100]}/>
+        <meshStandardMaterial />
+      </mesh>
+      <OrbitControls autoRotate/>
+    </Canvas>
+  )
+}
 
 export default App
