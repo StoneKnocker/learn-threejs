2443280ca92d3d3fc17fd29ae5bb9d7287b14ae1
xianyang <xianyang.lu@vcg.com>
2024-12-27 15:09:22 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './index.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { Suspense } from 'react'
 
 const App = () =>{
   return (
