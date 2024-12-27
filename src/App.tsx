b81318462a4002cccc8e72951010007e0498648d
xianyang <xianyang.lu@vcg.com>
2024-12-27 16:30:55 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -4,14 +4,22 @@ import { OrbitControls } from '@react-three/drei'
 
 const App = () =>{
   return (
-    <Canvas>
-      <ambientLight intensity={0.2} />
-      <directionalLight/>
+    <Canvas
+      camera={{
+        position: [1, 1, 1],
+        fov: 75,
+        near: 0.1,
+        far: 100,
+      }}
+    >
+      <color attach="background" args={[0,0,0]}/>
+      <OrbitControls />
       <mesh>
+        <ambientLight intensity={0.2}/>
+        <directionalLight position={[10,10,10]}/>
         <sphereGeometry args={[1, 100, 100]}/>
         <meshStandardMaterial />
       </mesh>
-      <OrbitControls autoRotate/>
     </Canvas>
   )
 }
