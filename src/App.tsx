70a2d2adb5004f6c6e09cf8b5b9197870e97f455
xianyang <xianyang.lu@vcg.com>
2024-12-27 14:34:19 +0800

    l08

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
 import './index.css'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
-import { Suspense } from 'react'
+import { Suspense, useRef, useState, useEffect } from 'react'
 
-const App = () => (
-  <Canvas>
-      <Suspense fallback={null}>
-        <ambientLight intensity={0.2} />
-        <directionalLight/>
-        <mesh>
-          <sphereGeometry args={[1, 100, 100]}/>
-          <meshStandardMaterial />
-        </mesh>
-        <OrbitControls autoRotate/>
-      </Suspense>
+const Cube = ({color}) => {
+  return (
+    <mesh position={[0,0,0]}>
+      <boxGeometry args={[1,1,1]} />
+      <meshBasicMaterial color={color} />
+    </mesh>
+  )
+}
+
+const App = () => {
+  const [isFullscreen, setIsFullscreen] = useState(false)
+  const canvasRef = useRef<HTMLCanvasElement | null>(null)
+  useEffect(() => {
+    if (canvasRef.current && document) {
+      if (isFullscreen) {
+        canvasRef.current?.requestFullscreen()
+      } else {
+        if (document.exitFullscreen) {
+          document?.exitFullscreen()
+        }
+      }
+    }
+  }, [isFullscreen])
+
+  return(
+  <Canvas
+  onDoubleClick={()=>setIsFullscreen(!isFullscreen)}
+  onCreated={(state) => {
+    canvasRef.current = state.gl.domElement
+  }}
+  camera={{
+    position: [0,0,3],
+    fov: 75,
+    aspect: 800/600
+  }}
+  >
+    <color attach="background" args={[0,0,0]} />
+    <OrbitControls autoRotate/>
+    <Cube color={0xff0000}/>
   </Canvas>
-)
+  );
+}
 
 export default App
