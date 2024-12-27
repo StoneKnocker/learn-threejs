c6471511e858226e05fea5041f406b973774c0d7
xianyang <xianyang.lu@vcg.com>
2024-12-27 17:11:34 +0800

    l12 materials

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
 import './index.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Plane, Sphere, Stage, Torus } from '@react-three/drei'
+import { useState } from 'react';
+import * as THREE from 'three';
+import { useControls } from 'leva';
+
+const Model = () => {
+  const materialProps = useControls({color:'aqua', opacity:1, roughness:0.5, metalness:0.5});
+  const [material, setMaterial] = useState<THREE.MeshPhysicalMaterial|undefined>(undefined); 
+  return (
+    <>
+     <meshPhysicalMaterial ref={setMaterial} side={THREE.DoubleSide} {...materialProps}/>
+     <Sphere args={[1,32,32]} material={material}/>
+     <Plane args={[2,2]} position={[3,0,0]} material={material}/>
+     <Torus args={[1, 0.5, 32, 32]} position={[-3.5,0,0]} material={material}/>
+    </>
+  )
+}
 
 const App = () =>{
   return (
@@ -11,15 +27,14 @@ const App = () =>{
         near: 0.1,
         far: 100,
       }}
+      shadows
+      dpr={[1,2]}
     >
       <color attach="background" args={[0,0,0]}/>
-      <OrbitControls />
-      <mesh>
-        <ambientLight intensity={0.2}/>
-        <directionalLight position={[10,10,10]}/>
-        <sphereGeometry args={[1, 100, 100]}/>
-        <meshStandardMaterial />
-      </mesh>
+      <OrbitControls makeDefault/>
+      <Stage intensity={1}>
+        <Model/>
+      </Stage>
     </Canvas>
   )
 }
