e13e80a77b96f344f3068c71a3d4a14b1038eab1
xianyang <xianyang.lu@vcg.com>
2024-12-28 21:02:52 +0800

    save

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,49 @@
 import './index.css'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Plane, Sphere, useHelper } from '@react-three/drei'
+import { useRef } from 'react'
+import { useControls } from 'leva'
+import * as THREE from 'three'
+
+const Model = () => {
+  return (
+    <>
+    <Sphere args={[1,32,32]} castShadow>
+      <meshPhysicalMaterial color="aqua"/>
+    </Sphere>
+    <Plane args={[20,20]} position={[0,-1,0]} receiveShadow rotation={[-Math.PI * -0.5,0,0]}>
+      <meshPhysicalMaterial color="gray"/>
+    </Plane>
+    </>
+  )
+}
+
+const Lights = () => {
+  const lightProps = useControls({
+    color: 'aqua',
+    intensity: 1,
+    position: [3,3,3],
+    distance: 10,
+    angle: Math.PI /4,
+    penumbra: 0.5,
+    decay: 0.5
+  })
+  const mainLightRef = useRef(null)
+  useHelper(mainLightRef, THREE.SpotLightHelper, 'red')
+  return (
+    <>
+    <ambientLight color='aqua'/>
+    <spotLight ref={mainLightRef} {...lightProps}/>
+    </>
+  )
+}
 
 const App = () =>{
   return (
-    <Canvas>
-      <ambientLight intensity={0.2} />
-      <directionalLight/>
-      <mesh>
-        <sphereGeometry args={[1, 100, 100]}/>
-        <meshStandardMaterial />
-      </mesh>
-      <OrbitControls autoRotate/>
+    <Canvas shadows dpr={[1,2]}>
+      <Lights/>
+      <Model/>
+      <OrbitControls makeDefault/>
     </Canvas>
   )
 }
